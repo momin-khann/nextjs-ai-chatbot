cc1f9515cb33df1899122b14d6258a3fbad0bdf1
Momin Khan <mominkhan.dev@gmail.com>
2024-11-27 21:40:36 +0500

    refactor code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { Chat } from "./components/chat";
+import {Chat} from "./components/chat";
 
 export const runtime = 'edge';
 
 export default function Page() {
-  return <Chat />;
+  return <Chat/>;
 }
